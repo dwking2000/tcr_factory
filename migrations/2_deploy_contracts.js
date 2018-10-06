0790b70c655c4a3de3e4ff0a422efce66c2bd868
Doug King <dwking@gmail.com>
2018-10-06 14:09:14 -0700

    TcrFactory skeleton

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-let BondingCurve = artifacts.require('./BondingCurve.sol');
+let TcrFactory = artifacts.require('./BondingCurve.sol');
 let ECRecovery = artifacts.require('zeppelin-solidity/contracts/ECRecovery.sol');
 
 module.exports = function (deployer) {
   deployer.deploy(ECRecovery);
-  deployer.link(ECRecovery, BondingCurve);
-  deployer.deploy(BondingCurve);
+  deployer.link(ECRecovery, TcrFactory);
+  deployer.deploy(TcrFactory);
 };
